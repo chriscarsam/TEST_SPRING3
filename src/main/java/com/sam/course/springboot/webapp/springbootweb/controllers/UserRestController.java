b2779770812be2277cf0d5c2673c316fbf85a243
Christian <chriscar.sam@gmail.com>
2023-12-23 18:15:22 -0500

    The DTO class

--- src/main/java/com/sam/course/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/sam/course/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -8,13 +8,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sam.course.springboot.webapp.springbootweb.models.User;
+import com.sam.course.springboot.webapp.springbootweb.models.dto.UserDto;
+
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
     @GetMapping("/details")
-    public Map<String, Object> details(){
+    public UserDto details() {
+        User user = new User("Isabella", "Tavarez");
+        UserDto userDto = new UserDto();
+        userDto.setUser(user);
+        userDto.setTitle("Hello world Spring Boot");
+        
+        return userDto;
+    }
+    
+
+    @GetMapping("/details-map")
+    public Map<String, Object> detailsMap(){
         User user = new User("Charly", "Test");
         Map<String, Object> body = new HashMap<>();
         body.put("title", "Hello world Spring Boot");
