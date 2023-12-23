701716a30a0cd8c7b0429ac6c34480f49f63b870
Christian <chriscar.sam@gmail.com>
2023-12-22 20:37:44 -0500

    Passing data to the view

--- src/main/java/com/sam/course/springboot/webapp/springbootweb/controllers/UserController.java
+++ src/main/java/com/sam/course/springboot/webapp/springbootweb/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.sam.course.springboot.webapp.springbootweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
      @GetMapping("/details")
-    public String details(){
+    public String details(Model model){
+        model.addAttribute("title", "Hello world Spring Boot");
+        model.addAttribute("name", "Charly");
+        model.addAttribute("lastname", "Test");
         return "details";
     }
 }
