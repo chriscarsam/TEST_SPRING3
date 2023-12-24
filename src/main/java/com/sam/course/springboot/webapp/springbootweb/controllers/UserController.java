9d20fff390c5165c0b4473ac0bec97108a775af9
Christian <chriscar.sam@gmail.com>
2023-12-24 14:49:12 -0500

    Lists in Thymeleaf

--- src/main/java/com/sam/course/springboot/webapp/springbootweb/controllers/UserController.java
+++ src/main/java/com/sam/course/springboot/webapp/springbootweb/controllers/UserController.java
@@ -1,7 +1,11 @@
 package com.sam.course.springboot.webapp.springbootweb.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.sam.course.springboot.webapp.springbootweb.models.User;
@@ -16,5 +20,14 @@ public class UserController {
         model.addAttribute("title", "Hello world Spring Boot");
         model.addAttribute("user", user);
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model){
+        List<User> users = new ArrayList<>();
+
+        model.addAttribute("users", users);
+        model.addAttribute("title", "List of users");
+        return "list";
     }
 }
