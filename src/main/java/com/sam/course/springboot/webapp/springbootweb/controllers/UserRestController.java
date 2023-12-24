6e1dc7c6c1ec1dae6c3e7bb95be3b71c0ace064f
Christian <chriscar.sam@gmail.com>
2023-12-24 14:23:30 -0500

    Api Rest list

--- src/main/java/com/sam/course/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/sam/course/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -1,6 +1,8 @@
 package com.sam.course.springboot.webapp.springbootweb.controllers;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +25,22 @@ public class UserRestController {
         userDto.setTitle("Hello world Spring Boot");
         
         return userDto;
+    }
+
+    @GetMapping("/list")
+    public List<User> list(){
+        User user = new User("Lorena", "Navarra");
+        User user2 = new User("Isabella", "Tabarez");
+        User user3 = new User("Tania", "Santo Domingo");
+
+        List<User> users = Arrays.asList(user, user2, user3);
+
+        // List<User> users = new ArrayList<>();
+        // users.add(user);
+        // users.add(user2);
+        // users.add(user3);
+
+        return users;
     }
     
 
