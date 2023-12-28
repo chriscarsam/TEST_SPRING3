2920711d17bb298dcbc9eed603f63a3b980dd9a6
Christian <chriscar.sam@gmail.com>
2023-12-28 15:41:02 -0500

    Request object from HttpServletRequest

--- src/main/java/com/sam/course/springboot/webapp/springbootweb/controllers/RequestParamsController.java
+++ src/main/java/com/sam/course/springboot/webapp/springbootweb/controllers/RequestParamsController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sam.course.springboot.webapp.springbootweb.models.dto.ParamDto;
 import com.sam.course.springboot.webapp.springbootweb.models.dto.ParamMixDto;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/params")
 public class RequestParamsController {
@@ -29,4 +31,16 @@ public class RequestParamsController {
         return params;
     }
 
+    @GetMapping("/request")
+    public ParamMixDto request(HttpServletRequest request){
+        Integer code = 0;
+        try {
+            code = Integer.parseInt(request.getParameter("code"));
+        } catch (Exception e) {
+        }
+        ParamMixDto params = new ParamMixDto();
+        params.setCode(code);
+        params.setMessage(request.getParameter("message"));
+        return params;
+    }
 }
