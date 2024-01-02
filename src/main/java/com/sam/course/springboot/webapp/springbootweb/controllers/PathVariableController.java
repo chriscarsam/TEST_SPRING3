60518e01ccf2066f8c1d44ed59429fd6c0866daa
Christian <chriscar.sam@gmail.com>
2024-01-02 16:46:43 -0500

    Api Rest send json POST request

--- src/main/java/com/sam/course/springboot/webapp/springbootweb/controllers/PathVariableController.java
+++ src/main/java/com/sam/course/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -5,9 +5,12 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sam.course.springboot.webapp.springbootweb.models.User;
 import com.sam.course.springboot.webapp.springbootweb.models.dto.ParamDto;
 
 @RestController
@@ -28,5 +31,11 @@ public class PathVariableController {
         json.put("id", id);
 
         return json;
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user){
+        user.setName(user.getName().toUpperCase());
+        return user;
     }
 }
