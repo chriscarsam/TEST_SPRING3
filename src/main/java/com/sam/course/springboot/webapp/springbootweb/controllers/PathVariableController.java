2a65528fb21db05e0c3c24246a11cc1d5973cf0e
Christian <chriscar.sam@gmail.com>
2024-01-03 19:50:32 -0500

    Spring Environment object to read configurations

--- src/main/java/com/sam/course/springboot/webapp/springbootweb/controllers/PathVariableController.java
+++ src/main/java/com/sam/course/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +46,9 @@ public class PathVariableController {
     @Value("#{${config.valuesMap}.price}")
     private Long price;
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message){
         ParamDto param = new ParamDto();
@@ -68,10 +73,13 @@ public class PathVariableController {
 
     @GetMapping("/values")
     public Map<String, Object> values(@Value("${config.message}") String message) {
+        Long code2 = environment.getProperty("config.code", Long.class);
         Map<String, Object> json = new HashMap<>();
         json.put("code", code);
         json.put("username", username);
         json.put("message", message);
+        json.put("message2", environment.getProperty("config.message"));
+        json.put("code2", code2);
         json.put("listOfValues", listOfValues);
         json.put("valueList", valueList);
         json.put("valueString", valueString);
